5b6fc86774bf9d58285140f1058219ac74b8c6bb
boraxpr <tata-naipawat@hotmail.com>
2022-07-06 17:30:47 +0700

    Update 3 : Clear all TODOs, Adjust view
    The calculator is now fully functional.
    Adjusted view : text size, screen size,
    Changed to Double type (From float).

--- app/src/main/java/com/boraxpr/kotlin_calculator/MainActivity.kt
+++ app/src/main/java/com/boraxpr/kotlin_calculator/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.boraxpr.kotlin_calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.isDigitsOnly
-import androidx.core.view.WindowInsetsCompat
 import com.boraxpr.kotlin_calculator.databinding.ActivityMainBinding
 import com.google.android.material.button.MaterialButton
-import kotlin.math.ceil
+import java.math.BigInteger
 
 
 class MainActivity : AppCompatActivity() {
@@ -68,24 +67,32 @@ class MainActivity : AppCompatActivity() {
         val textInputScreen = binding.textViewInputScreen
         val textHistoryScreen = binding.textViewHistory
         //Calculation-related variables
-        var operandA: Float? = null
-        var operandB: Float? = null
+        var operandA: Double? = null
+        var operandB: Double? = null
         var operation: MaterialButton? = null
         var continuous = 0
-        var contValue : Float? = null
-
+        var contValue : Double? = null
         for (button in buttonArr) {
             button.setOnClickListener {
                 if (button.text.equals(buttonAC.text)) {
                     textInputScreen.text = ""
+                    textHistoryScreen.text = ""
                     operandA = null
                     operandB = null
                     operation = null
                     continuous = 0
                     contValue = null
                 } //INPUTS
-                else if (button.text.isDigitsOnly()) {
+                else if (button.text.isDigitsOnly() && textInputScreen.text.removePrefix("-").length < 9) {
+//                    When typed a zero first into a whole number, ignore zero. Allow a zero only when followed by a decimal
+                    if (textInputScreen.text.toString() == "0" && isWhole(textInputScreen.text.toString().toDouble())){
+                        textInputScreen.text = ""
+                    }
                     textInputScreen.append(button.text.toString())
+                    //Continuous calculation : Catch any changed operandB
+                    if (contValue != null) {
+                        operandB = textInputScreen.text.toString().toDouble()
+                    }
                 } //NEGATIVE
                 else if (button == buttonNegative && textInputScreen.text.isNotEmpty()) {
                     if (textInputScreen.text.startsWith("-")) {
@@ -97,47 +104,83 @@ class MainActivity : AppCompatActivity() {
                 } //OPERATIONS
                 else if (button == buttonModulo || button == buttonDivide || button == buttonPlus
                     || button == buttonMinus || button == buttonMultiply){
-
                     if(textInputScreen.text.isNotEmpty()){
-                        operandA = textInputScreen.text.toString().toFloat()
+                        operandA = textInputScreen.text.toString().toDouble()
                     }
                     operation = button
                     textInputScreen.text = ""
                 } //DECIMAL
                 else if (button == buttonDecimal && !textInputScreen.text.contains(".")){
                     textInputScreen.append(button.text.toString())
-                } //CALCULATION TODO: Bug Fix After show a History, Pressing an Equal crashes the app
+                } //CALCULATION
                 else if (button == buttonEqual){
+
                     if (operandA == null && operandB == null){
                         textInputScreen.text = ""
+                        textHistoryScreen.text = ""
                     }
                     else {
                         if (operandB == null && continuous == 0){
-                            operandB = textInputScreen.text.toString().toFloat()
+                            operandB = textInputScreen.text.toString().toDouble()
                             continuous = 1
                         }
                         else {
                             operandA = contValue
                         }
-                        var showA = operandA.toString()
-                        var showB = operandB.toString()
-                        //TODO:Not working properly. whole values still show decimal
-                        if (!isWhole(operandA!!)){
-                            showA = "%.3f".format(operandA)
-                        }
-                        if (!isWhole(operandB!!)){
-                            showB = "%.3f".format(operandB)
-                        }
+
+                        // Dealing with rounding (Limit to 3 decimal places) and if Whole number,then not showing decimal places
+                        val showA = digitsFormatting(operandA!!)
+                        val showB = digitsFormatting(operandB!!)
 
                         textHistoryScreen.text = getString(R.string.history, showA, operation!!.text, showB)
-                        //TODO:Calculations and show result on the input screen
+
+                        //Calculate and Show output on the screen
+                        val calculation = calculate(operation!!,operandA!!,operandB!!)
+                        textInputScreen.text = digitsFormatting(calculation)
+                        contValue = calculation
                     }
                 }
             }
         }
     }
-    private fun isWhole(float: Float): Boolean {
-        return float.rem(1).equals(0.0)
+    private fun isWhole(float: Double): Boolean {
+        return float % 1 == 0.0
+    }
+    private fun digitsFormatting(float: Double): String {
+        var digits = float.toString()
+//        if (!isWhole(float) && digits.split(".")[1].length > 3){
+//            digits = "%.3f".format(float)
+//        }
+//        if (isWhole(float)){
+//            digits =  float.toString()
+//        }
+        return digits
+    }
+    private fun calculate(operation: MaterialButton, operandA: Double, operandB: Double): Double{
+        var calculation = 0.0
+        val buttonModulo = binding.buttonModulo
+        val buttonDivide = binding.buttonDivide
+        val buttonMultiply = binding.buttonMultiply
+        val buttonMinus = binding.buttonMinus
+        val buttonPlus = binding.buttonAdd
+        when (operation) {
+            buttonPlus -> {
+                calculation = operandA + operandB
+            }
+            buttonMinus -> {
+                calculation = operandA - operandB
+            }
+            buttonMultiply -> {
+                calculation = operandA * operandB
+            }
+            buttonDivide -> {
+                calculation = operandA / operandB
+            }
+            buttonModulo -> {
+                calculation = operandA % operandB
+            }
+        }
+        return calculation
     }
 }
 
